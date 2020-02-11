54813b372656676e494825d78c5af58b9aaeee52
Your Name <you@example.com>
2020-02-11 10:55:47 -0500

    updates

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 const paragraph = document.querySelector('p');
-paragraph.style.fontSize = '1em';
+paragraph.style.fontSize = '10em';
 paragraph.style.color = 'orange';
 paragraph.style.transform = 'rotate(180deg)'
 
 
 const h1 = document.querySelector('h1');
 h1.style.color = 'blue';
-h1.style.fontsize = '4em'
-h1.style.transform = 'rotate(90deg)'
+h1.style.transform = 'rotate(90deg)';
 
 const h3 = document.querySelector('h3');
-h3.style.color = 'pink'
-h3.style.fontStretch ='ultra condensed'
+h3.style.color = 'brown';
+h3.style.fontSize ='40em'
 
-const newli = document.querySelector('ol');
-newli.style.color = 'orange';
-newli.style.fontSize = '5em';
+
+
+
+const newli = document.querySelector('li');
+newli.style.color = 'limegreen';
+newli.style.fontSize = '10em'
+
 
 
 
