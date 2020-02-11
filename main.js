0053a1100f44aebd83a5ecf62301766001ab3114
Your Name <you@example.com>
2020-02-11 10:00:08 -0500

    updates

---
+++ main.js
@@ -0,0 +1,19 @@
+const paragraph = document.querySelector('p');
+paragraph.style.fontSize = '2em';
+paragraph.style.color = 'pink';
+
+const h1 = document.querySelector('h1');
+h1.style.color = 'blue';
+h1.style.fontsize = '1em'
+h1.style.transform = 'rotate(180deg)'
+
+const newli = document.createElement('li');
+newli.style.color = 'orange';
+
+
+
+
+document.body.style.backgroundColor = 'yellow';
+
+
+
